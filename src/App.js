d86c4e00d7794e80190417da97b1a7de18f23c4d
Kenna Taddese Roba <KennaTaddese@cbe.com.et>
2024-02-21 12:05:19 +0300

    add a responsive header to page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>
+        <li> Home</li>
+        <li> Portfolio </li>
+        <li> Works </li>
+        <li>
+          {" "}
+          Contact
+          <div class="dropDownContainer">
+            <div class="content"> Phone </div>
+            <div class="content emailContainer">
+              {" "}
+              Email
+              <div> </div>
+              <div class="emailDropDown"> kennataddese6@gmail.com </div>
+            </div>
+            <div class="content"> Address </div>
+          </div>
+        </li>
+        <li>
+          {" "}
+          <> About </>{" "}
+        </li>
+        <li className="HireMeright">
+          {" "}
+          <> Hire Me </>{" "}
+        </li>
+      </ul>
+      <h1> Hello my name is kenna 1</h1>
+      <h1> Hello my name is kenna 2</h1>
+      <h1> Hello my name is kenna 3</h1>
+      <h1> Hello my name is kenna 4 </h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+      <h1> Hello my name is kenna</h1>
+    </>
   );
 }
 
