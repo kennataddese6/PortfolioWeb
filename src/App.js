9d09f40596785491e77e8cbd18bb8dd9e4f3c6ef
Kenna Taddese Roba <KennaTaddese@cbe.com.et>
2024-02-24 12:05:57 +0300

    add a list menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import "./App.css";
 import { FaAngleRight } from "react-icons/fa";
 import imageOne from "../src/Assets/ImageOne.jpeg";
+import { useState } from "react";
 function App() {
+  const [showNavbar, setShowNavbar] = useState(false);
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar);
+  };
+
   return (
     <>
-      <ul>
+      <div
+        className="menuContainer"
+        onClick={() => {
+          handleShowNavbar();
+        }}
+      >
+        <div className="menulist"> </div>
+        <div className="menulist"> </div>
+        <div className="menulist"> </div>
+      </div>
+      <ul className={showNavbar ? "showNavbar" : ""}>
         <li> Home</li>
         <li> Portfolio </li>
         <li> Works </li>
@@ -34,9 +50,13 @@ function App() {
           <> Hire Me </>{" "}
         </li>
       </ul>
-      <h1> Hello</h1>
+      <h6>.</h6>
       <div className="gallery">
-        <div className="descriptionContainer"> Hello</div>
+        <div className="descriptionContainer">
+          <h1 className="IntroductionName">Hi. This is kenna.</h1>
+          <h1 className="IntroductionCareer">I am a software developer.</h1>
+          <button className="learnMoreButton">Learn More</button>
+        </div>
       </div>
       <div className="gallery">
         <img src={imageOne} className="LandingPageImage" />
